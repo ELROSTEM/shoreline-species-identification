7493e7ed711c42159ea243b7a8683e4d327da538
irrelevantRyan <ryanz22@nycstudents.net>
2022-02-05 14:44:46 -0500

    Shellfish App

--- app.py
+++ app.py
@@ -9,44 +9,20 @@ from PIL import Image
 
 st.title("Species Detection")
 
-#----------------------------------------------------------
-#Form
 
-form = st.empty()
-with form.container():
-    # Form
-    with st.form(key='species_form'):
-        
-        # Wall section input for where the shellfish was found.
-        # I want to have like an animation of the selection process. So if 1 section 1 will light up
-        st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: center;} </style>', unsafe_allow_html=True)
-        st.write('<style>div.st-bf{flex-direction:column;} div.st-ag{font-weight:bold;padding-left:2px;}</style>', unsafe_allow_html=True)
-        wall_section = st.radio("Wall Section",("1","2","3"))
+# Take Picture
+picture = st.camera_input("Take a picture")
+# or use file uploader
+if picture is None:
+    picture = st.file_uploader("Upload an image")
+    st.caption("Unable to use the camera? Use the file uploader and select take photo.")
 
-
-        # Take Picture
-        picture = st.camera_input("Take a picture")
-        # or use file uploader
-        if picture is None:
-            picture = st.file_uploader("Upload an image")
-        st.caption("Unable to use the camera? Use the file uploader and select take photo.")
-
-        # Submit button
-        submit_button = st.form_submit_button(label='Submit')
-
-if submit_button == True:
-    form.empty()
-
-    # AI --------------------------------------
-
+if picture is not None:
     # Read Image as PIL
     img = Image.open(picture)
 
-    # Save Image
-    # save_img = img.save("img.jpg")
-
     # Load Model
-    model = torch.jit.load('./model/model_scripted.pt')
+    model = torch.jit.load('./model/shellfish_model_scripted.pt')
     model.eval()
 
     ## Preprocess to tensor
@@ -64,6 +40,63 @@ if submit_button == True:
     # # Predicted class value using argmax
     _, preds = torch.max(prediction, 1)
 
+    st.success("Indentified!")
+
+    # Display info to user
+    st.header("So what did you detect?")
+    if preds == 1:
+        st.session_state['animal'] = 'Hard Clam'
+        st.write("Hard Clam")
+    else:
+        st.session_state['animal'] = 'Atlantic Bay Scallop'
+        st.write("Atlantic Bay Scallop")
+    st.image(img, use_column_width=True, clamp= True)
+
+    st.header("Here is information on your species")
+    st.header("Also here is a graph of how many times your species was detected at our park")
+
+
+#----------------------------------------------------------
+#Form
+
+if 'animal' in st.session_state:
+    form = st.empty()
+    with form.container():
+        # Form
+        with st.form(key='species_form'):
+
+            st.header("Sighting")
+            st.write(f"Animal Sighted: {st.session_state.animal}")
+            
+            st.header("Location")
+            # Wall section input for where the shellfish was found.
+            # I want to have like an animation of the selection process. So if 1 section 1 will light up
+            st.write('<style>div.row-widget.stRadio > div{flex-direction:row;justify-content: center;} </style>', unsafe_allow_html=True)
+            st.write('<style>div.st-bf{flex-direction:column;} div.st-ag{font-weight:bold;padding-left:2px;}</style>', unsafe_allow_html=True)
+            wall_section = st.radio("Wall Section",("1","2","3"))
+
+            # Submit button
+            submit_button = st.form_submit_button(label='Submit')
+
+    if submit_button == True:
+        form.empty()
+
+        date = datetime.now().strftime("%d/%m/%Y")
+        time = datetime.now().strftime("%H:%M:%S")
+
+        # Write in database
+            # Code
+
+        st.success(f"Submission Time: {date} {time}. Thank you for filling out the form! Hope you enjoyed the park!")
+        st.balloons()
+        
+
+
+
+# Green detection using OpenCv
+    # Save Image
+    # save_img = img.save("img.jpg")
+
     # # Green
     # # # Read Image
     # img = cv.imread("img.jpg")
@@ -77,32 +110,8 @@ if submit_button == True:
     # green[imask] = img[imask]
     # # ## save 
     # cv.imwrite("green.png", green)
-
-    # -------------------------------------------------------
-    # Info write / Display
-
-    date = datetime.now().strftime("%d/%m/%Y")
-    time = datetime.now().strftime("%H:%M:%S")
+    # st.image(green, use_column_width=True,clamp = True)
 
     #Write the data into database
     # body = [date, time, age, alge, shape, review]
     # wks.append_row(body, table_range="A1:G1")
-
-    #Gets rid of form so user can't submit twice
-    st.success(f"Submission Time: {date} {time}. Thank you for filling out the form!")
-    st.balloons()
-    
-    # Display info to user
-    st.header("So what did you detect?")
-    if preds == 1:
-        st.write("Bee")
-    else:
-        st.write("Ant")
-    st.image(img, use_column_width=True, clamp= True)
-    # st.image(green, use_column_width=True,clamp = True)
-
-    st.header("Here is information on your species")
-    st.header("Also here is a graph of how many times your species was detected at our park")
-    st.markdown("Hope you enjoyed the park!")
-
-
